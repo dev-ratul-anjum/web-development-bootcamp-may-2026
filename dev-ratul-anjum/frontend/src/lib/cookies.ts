0ffd03ea5f47447a83c0362cb290b96d20f75de8
Ratul Anjum <206848890+dev-ratul-anjum@users.noreply.github.com>
2026-05-14 16:26:52 +0600

    Add debug logs for cookieStore and cookieHeader
    Added console logs to debug cookieStore and cookieHeader.

--- dev-ratul-anjum/frontend/src/lib/cookies.ts
+++ dev-ratul-anjum/frontend/src/lib/cookies.ts
@@ -2,7 +2,9 @@ import { cookies } from "next/headers";
 
 export async function getDecodedCookies(): Promise<string> {
   const cookieStore = await cookies();
+  console.log("cookieStore main : ", cookieStore);
   const cookieHeader = cookieStore.toString();
+    console.log("cookieHeader main : ", cookieHeader);
 
   return cookieHeader;
 }
